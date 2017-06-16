3bbec6a8a438940b55402b4beda89821a3c8eee0
Mark Forsyth <shadowfire168@outlook.com>
2017-06-16 14:29:25 +0200

    Updated and removed console output

--- index.dart
+++ index.dart
@@ -3,9 +3,10 @@ import 'dart:html';
 void main() {
   // Name by age - Dart & HTML5 Edition
 
-  // Import input boxes into dart
+  // Import input and output boxes into dart
   InputElement getName = querySelector("#get-name");
   InputElement getAge = querySelector("#get-age");
+  var output = querySelector('#output');
 
   // Set reciever variables
   String name;
@@ -15,21 +16,25 @@ void main() {
   getName.onChange.listen((_) {
     name = getName.value;
     if (name != null && age != null) {
+      output.children.clear();
+      
       for (var i = 0; i < age; i++) {
-        print(name);
+        var newName = new ParagraphElement();
+        newName.text = name;
+        output.children.add(newName);
       }
-    } else {
-      print('You haven\'t entered something in all fields yet.');
     }
   });
   getAge.onChange.listen((_) {
     age = int.parse(getAge.value);
     if (name != null && age != null) {
+      output.children.clear();
+      
       for (var i = 0; i < age; i++) {
-        print(name);
+        var newName = new ParagraphElement();
+        newName.text = name;
+        output.children.add(newName);
       }
-    } else {
-      print('You haven\'t entered something in all fields yet.');
     }
   });
 }
